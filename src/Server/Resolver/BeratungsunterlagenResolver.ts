a6fc60c9468476eb06f021ef2e7860f1088afe30
Achim <achim@zibra.de>
2019-10-29 00:50:51 +0100

    Entfernt debug logs und übernimmt searchText aus query parameter

--- src/Server/Resolver/BeratungsunterlagenResolver.ts
+++ src/Server/Resolver/BeratungsunterlagenResolver.ts
@@ -6,15 +6,15 @@ export class BeratungsunterlagenResolver {
 
     /**
      * Method resolves data for timeline API
-     * @param search 
+     * @param searchText 
      */
-    public async resolve(search: string) {
+    public async resolve(searchText: string) {
 
         // We use "Suche" client
         let ksdSucheClient = new KsdSucheClient();
 
         // 
-        let bodyHtml = await ksdSucheClient.submitSearch('Stuttgart 28');   // FIXME
+        let bodyHtml = await ksdSucheClient.submitSearch(searchText);
         
         // Parse html with cheerio
         let $ = cheerio.load(bodyHtml);     // We parse dom here an pass dom to scrapers instead of HTML string
--- src/Server/Resolver/TimelineResolver.ts
+++ src/Server/Resolver/TimelineResolver.ts
@@ -6,16 +6,16 @@ export class TimelineResolver {
 
     /**
      * Method resolves data for timeline API
-     * @param search 
+     * @param searchText 
      */
-    public async resolve(search: string) {
+    public async resolve(searchText: string) {
 
         // Define response object stub
         // @see https://timeline.knightlab.com/docs/json-format.html
         let response: any = {
             "title": {
                 "text": {
-                    "headline": "Suche: "+ search,
+                    "headline": "Suche: "+ searchText,
                     "text": "<p>Alles was wir dazu finden können</p>"
                 }
             }, 
@@ -26,7 +26,7 @@ export class TimelineResolver {
         let ksdSucheClient = new KsdSucheClient();
 
         // 
-        let bodyHtml = await ksdSucheClient.submitSearch('Stuttgart 28');
+        let bodyHtml = await ksdSucheClient.submitSearch(searchText);
         
         // Parse html with cheerio
         let $ = cheerio.load(bodyHtml);     // We parse dom here an pass dom to scrapers instead of HTML string
@@ -34,8 +34,6 @@ export class TimelineResolver {
         // Scrape retrieved bodyHTML
         let buchScraper = new BeratungsunterlagenScraper();
         let beratungsunterlagenArr = buchScraper.scrape($);
-
-        console.log(beratungsunterlagenArr);
 
         // Push each "Beratungsunterlage" into response object
         beratungsunterlagenArr.forEach((berObj) => {
