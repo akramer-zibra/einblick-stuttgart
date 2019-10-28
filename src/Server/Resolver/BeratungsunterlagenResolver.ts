9224263b9c4aca081e789e9563f94bf806446553
Achim <achim@zibra.de>
2019-10-29 00:11:07 +0100

    Ergänzt GraphQL um zusätzliche Query für Beratungsunterlagen

---
+++ src/Server/Resolver/BeratungsunterlagenResolver.ts
@@ -0,0 +1,33 @@
+import cheerio from 'cheerio';
+import { KsdSucheClient } from "../DataSources/Ratsdokumente/KsdSucheClient";
+import { BeratungsunterlagenScraper } from '../DataSources/Ratsdokumente/Scraper/BeratungsunterlagenScraper';
+
+export class BeratungsunterlagenResolver {
+
+    /**
+     * Method resolves data for timeline API
+     * @param search 
+     */
+    public async resolve(search: string) {
+
+        // DEBUG
+        console.log(search);
+        // DEBUG
+
+        // We use "Suche" client
+        let ksdSucheClient = new KsdSucheClient();
+
+        // 
+        let bodyHtml = await ksdSucheClient.submitSearch('Stuttgart 28');   // FIXME
+        
+        // Parse html with cheerio
+        let $ = cheerio.load(bodyHtml);     // We parse dom here an pass dom to scrapers instead of HTML string
+
+        // Scrape retrieved bodyHTML
+        let berScraper = new BeratungsunterlagenScraper();
+        let beratungsunterlagenArr = berScraper.scrape($);
+
+        // ..simply return scraped data
+        return beratungsunterlagenArr;
+    }
+}
