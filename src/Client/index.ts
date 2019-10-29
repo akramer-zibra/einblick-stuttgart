5177c58b00e93b1c1531aa5c05fcbf1c99ef9bed
Achim <achim@zibra.de>
2019-10-29 21:24:10 +0100

    Verschiebt API call in KeywordInput Komponente

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -1,9 +1,16 @@
 
 import { Timeline } from './ts/Timeline';
+import { KeywordInput } from './ts/KeywordInput';
+import { GraphQLClient } from './ts/GraphQLClient';
 
 const main = () => {
     console.log('Client Applikation läuft...');
 
+    // Initialize internal components
+    const graphQLCLient = new GraphQLClient();
+
+    // Initialize UI components
     const timeline = new Timeline();
+    new KeywordInput(graphQLCLient, timeline);
 }
 main();
--- src/Client/ts/Timeline.ts
+++ src/Client/ts/Timeline.ts
@@ -1,44 +1,14 @@
 import { GraphQLClient } from "./GraphQLClient";
-import gql from "graphql-tag";
 
 declare const TL: any;  // Declares global TL object integrated with linked script file in index.html
 
 export class Timeline {
-
-    /** Reference to this page's timeline object */
-    private timeline: any;
 
     /** Reference to graphQL client */
     private graphQLClient: GraphQLClient = new GraphQLClient();
 
     constructor() {
         console.log('Timeline initialisiert...');
-
-        // 
-        this.graphQLClient = new GraphQLClient();
-
-        // TEST
-        this.graphQLClient.client
-            .query({
-                query: gql`
-                    query {
-                        timelineAll(search:"Stuttgart 28") {
-                            events {
-                                media {url, caption}
-                                start_date {year, month, day}
-                                text {headline, text}
-                            }
-                        }
-                    }`
-            })
-            .then(result => {
-                console.log(result)
-            
-                // Update Timeline
-                this.update(result.data.timelineAll);
-            });
-            // TEST
-
     }
     
     /**
@@ -48,7 +18,7 @@ export class Timeline {
     update(timelineJson: any) {
 
         // Create a new Timeline object with given JSON
-        this.timeline = new TL.Timeline('timeline-embed', timelineJson, {
+        new TL.Timeline('timeline-embed', timelineJson, {
                 start_at_end: true,
                 timenav_height: 300,
                 language: 'de',
