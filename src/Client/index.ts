33bc8fe464ddaa50d6f38220b9bd87deef1e9f2a
Achim <achim@zibra.de>
2019-10-29 11:02:33 +0100

    Verbessert Timeline js integration in ts Klasse

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -1,7 +1,10 @@
 
+import { Timeline } from './ts/Timeline';
+
 const main = () => {
-
     console.log('Client Applikation läuft...');
-}
 
+    const timeline = new Timeline();
+    timeline.init();
+}
 main();
---
+++ src/Client/ts/Timeline.ts
@@ -0,0 +1,24 @@
+declare const TL: any;  // Declares global TL object from linked script file
+
+export class Timeline {
+
+    /** Reference to this page's timeline object */
+    private timeline: any;
+
+    init() {
+
+        // 
+        const timelineJson = {}; // replace make_the_json() with the JSON object you created
+
+        // two arguments: the id of the Timeline container (no '#')
+        // and the JSON object or an instance of TL.TimelineConfig created from
+        // a suitable JSON object
+        this.timeline = new TL.Timeline('timeline-embed', timelineJson, {
+                start_at_end: true,
+                default_bg_color: {r:0, g:0, b:0},
+                timenav_height: 250
+            });
+
+        console.log('Timeline initialisiert...');
+    }
+}
