3bac2fcee96159458a5a4cd34b50e68547f55433
Achim <achim@zibra.de>
2019-10-31 20:24:03 +0100

    Ergänzt client code um SearchController

---
+++ src/Client/ts/controller/SearchController.ts
@@ -0,0 +1,56 @@
+import { RatsdokumenteProvider } from "../provider/Ratsdokumente.provider";
+import { Timeline } from "../ui/Timeline";
+import { ToastFeedback } from "../ui/ToastFeedback";
+
+export class SearchController {
+
+    /** Referenzen zu notwendigen Objekten */
+    private ratsdokumenteProvider;
+    private timeline;
+
+    /**
+     * Konstruktor
+     * @param ratsdokumenteProvider 
+     * @param timeline 
+     */
+    constructor(ratsdokumenteProvider: RatsdokumenteProvider, timeline: Timeline) {
+
+        // Abhängikeiten injizieren
+        this.ratsdokumenteProvider = ratsdokumenteProvider;
+        this.timeline = timeline;
+    }
+
+    /**
+     * Methode führt die Suche mit dem übergebenen Text aus
+     * @param searchtext 
+     */
+    search(searchtext: string): Promise<number> {
+
+        return new Promise((resolve, reject) => {
+
+            // Benutze den Ratsdokumente-Provider um an die Daten aus der GraphQL API zu kommen
+            this.ratsdokumenteProvider
+                .queryRatsdokumenteByText(searchtext)
+                .then((apiData) => {
+
+                    // Überprüfe, ob überhaupt Ergebnisse vorhanden sind
+                    if(apiData.ratsdokumente.length === 0) {
+                        this.handleEmptyResult();
+                        resolve(0);
+                    }
+
+                    this.timeline.updateWithApiData(apiData.ratsdokumente);     // Übergebe die API Daten an die Timeline Instanz, um zu aktualisieren
+                    resolve(apiData.ratsdokumente.length);
+                })
+                .catch(reject);
+        });
+
+    }
+    
+    /**
+     * Methode reagiert auf ein leeres Ergebnis
+     */
+    private handleEmptyResult() {
+        ToastFeedback.showWarningToast("Es wurden keine Dokumente gefunden...");
+    }
+}
