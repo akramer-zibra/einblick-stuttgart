b3698c22b8b1d7b103a46eaa382e84aaa64a8033
Achim <achim@zibra.de>
2019-11-06 21:39:49 +0100

    Ergänzt Clientcode um TypeInput Klasse die Dokumenttypen ausliest

---
+++ src/Client/ts/ui/input/TypeInput.ts
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+
+export class TypeInput {
+
+    /**
+     * Methode lädt die Werte der ausgewählten Dokumenttypen
+     * und gibt diese zurück
+     */
+    values(): string[] {
+        const result: string[] = [];
+
+        // Wir selektieren alle ausgewählten Typen und ermitteln deren Werte
+        $('.app__types:checked').toArray().forEach(element => {
+            console.log(element);
+            result.push($(element).attr('data-type'));
+        });
+        return result;
+    }
+}
