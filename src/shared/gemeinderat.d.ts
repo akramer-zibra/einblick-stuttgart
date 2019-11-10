0a048967ecea02fad2f7a41e639c9b2f660e36c1
Achim <achim@zibra.de>
2019-11-10 17:57:15 +0100

    Vereinfacht Typ Person und Referenz zu Fraktion Typ

--- src/shared/gemeinderat.d.ts
+++ src/shared/gemeinderat.d.ts
@@ -2,10 +2,6 @@ export interface Person {
     name: string,
     funktion?: string, 
     partei?: string,     // Optional, weil nicht alle eteiligten (Bsp. OB Kuhn, Verwaltungsmitarbeitende, ...) keine Partei bzw. Fraktion besitzen
-    fraktion?: Fraktion, // s.o.
+    fraktion?: string,   // s.o.
     profilseite?: string
-}
-
-export interface Fraktion {
-    name: string
 }
