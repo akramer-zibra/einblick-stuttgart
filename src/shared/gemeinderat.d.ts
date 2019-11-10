e54996435000dc67a6145dbf37041ae304ac34ba
Achim <achim@zibra.de>
2019-11-10 17:38:03 +0100

    Ergänzt Typ Person um funktion-Feld

--- src/shared/gemeinderat.d.ts
+++ src/shared/gemeinderat.d.ts
@@ -1,5 +1,6 @@
 export interface Person {
     name: string,
+    funktion?: string, 
     partei?: string,     // Optional, weil nicht alle eteiligten (Bsp. OB Kuhn, Verwaltungsmitarbeitende, ...) keine Partei bzw. Fraktion besitzen
     fraktion?: Fraktion, // s.o.
     profilseite?: string
