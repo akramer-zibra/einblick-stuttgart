e91c04934d397ca43c2113a4f814c227291bf59f
Achim <achim@zibra.de>
2019-11-10 17:37:27 +0100

    Ergänzt Persontyp um Funktion-Feld

---
+++ src/Server/Gemeinderat/resolver/Gemeinderat.resolver.ts
@@ -0,0 +1,41 @@
+import cheerio from 'cheerio';
+import { Resolver } from '../../interfaces';
+import { GemeinderatClient } from '../data/html/GemeinderatClient';
+import { UebersichtPersonenScraper } from '../scraper/UebersichtPersonen.scraper';
+
+export class GemeinderatResolver implements Resolver {
+
+    /** Referenz zu abhängigen Objekten werden im Konstruktor direkt gesetzt */
+    
+    /**
+     * Factory Methode lädt Abhängigkeiten aus übergebenem IoC container
+     * @param container 
+     */
+    static build(container) {
+        return new GemeinderatResolver(container.GemeinderatClient,
+                                       container.UebersichtPersonenScraper);
+    } 
+
+    /**
+     * Konstruktor
+     * @param htmlClient
+     */
+    private constructor(private htmlClient: GemeinderatClient,
+                        private uebersichtPersonenScraper: UebersichtPersonenScraper) {
+    }
+
+    /**
+     * Methode löst die entsprechende GraphQL query auf
+     */
+    async resolve() {
+
+        // Wir lassen den HTML Client alle Gemeinderatsmitglieder laden
+        const bodyHtml = await this.htmlClient.getMitgliederGemeinderat();
+
+        // Parse die HTTP Antwort mit cheerio
+        const $ = cheerio.load(bodyHtml);     // Wir parsen den DOM, um auf die HTML Daten einfache rzugreifen zu können
+
+        // Scrape Mitglieder und gib die Arraycollection weiter
+        return this.uebersichtPersonenScraper.scrape($);
+    }
+}
