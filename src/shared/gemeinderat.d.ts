d80299169f2de47d482fe2783a5992816afbe420
Achim <achim@zibra.de>
2019-11-10 17:16:51 +0100

    Erweitert Typdokumente der Gemeinderat Informationen

--- src/shared/gemeinderat.d.ts
+++ src/shared/gemeinderat.d.ts
@@ -1,8 +1,8 @@
 export interface Person {
     name: string,
-    partei: string,
-    fraktion: Fraktion,
-    profilseite: string
+    partei?: string,     // Optional, weil nicht alle eteiligten (Bsp. OB Kuhn, Verwaltungsmitarbeitende, ...) keine Partei bzw. Fraktion besitzen
+    fraktion?: Fraktion, // s.o.
+    profilseite?: string
 }
 
 export interface Fraktion {
